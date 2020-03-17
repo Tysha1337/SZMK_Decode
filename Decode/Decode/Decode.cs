f9519f227707f24ac8dca98ef0b25168a85574d6
Tysha1337 <thedreamkill@gmail.com>
2020-03-17 19:00:20 +0700

    Исправлено распознование

--- Decode/Decode/Decode.cs
+++ Decode/Decode/Decode.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
-using Bytescout.BarCodeReader;
 using System.Threading.Tasks;
 using System.Drawing;
+using ZXing;
 
 namespace Decode
 {
@@ -15,86 +15,82 @@ namespace Decode
         {
             try
             {
-                using (Reader reader = new Reader())
+                Image myImage = Image.FromFile(path);
+                Bitmap source = new Bitmap(myImage);
+                myImage.Dispose();
+
+                int imgw = 0;
+                int imgh = 0;
+                BarcodeReader reader = new BarcodeReader();
+                string cash = null;
+
+                for (int i = 200; i < source.Width; i = i + 10)
                 {
-                    Image myImage = Image.FromFile(path);
-                    Bitmap source = new Bitmap(myImage);
-
-                    int imgw = 0;
-                    int imgh = 0;
-
-                    reader.BarcodeTypesToFind.DataMatrix = true;
-
-                    string cash = null;
-
-                    for (int i = 250; i < source.Width; i = i + 125)
+                    for (int j = 200; j < source.Height; j = j + 10)
                     {
-                        for (int j = 250; j < source.Height; j = j + 125)
+                        imgw = i; imgh = j;
+
+                        Bitmap bmp = source.Clone(new System.Drawing.Rectangle(source.Width - imgw, source.Height - imgh, 175, 200), source.PixelFormat);
+
+                        Result result = reader.Decode(bmp);
+
+                        if (result != null&& result.Text.Split('_').Length==6)
                         {
-                            imgw = i; imgh = j;
+                            cash = result.Text;
 
-                            Bitmap bmp = source.Clone(new System.Drawing.Rectangle(source.Width - imgw, source.Height - imgh, 250, 250), source.PixelFormat);
+                            var fromEncodind = Encoding.GetEncoding("ISO-8859-1");//из какой кодировки
+                            var bytes = fromEncodind.GetBytes(cash);
+                            var toEncoding = Encoding.GetEncoding(1251);//в какую кодировку
 
-                            FoundBarcode[] barcodes = reader.ReadFrom(bmp);
+                            cash = toEncoding.GetString(bytes);
 
-                            foreach (FoundBarcode code in barcodes)
+                            while (cash.IndexOf("<FNC1>") != -1)
                             {
-                                cash = code.Value;
+                                cash = cash.Replace("<FNC1>", "и");
                             }
-                            if (cash != null)
+
+                            String[] ExistingCharaterEnglish = new String[] { "A", "a", "B", "C", "c", "E", "e", "H", "K", "M", "O", "o", "P", "p", "T" };
+
+                            String[] ExistingCharaterRussia = new String[] { "А", "а", "В", "С", "с", "Е", "е", "Н", "К", "М", "О", "о", "Р", "р", "Т" };
+
+                            String[] Temp = cash.Split('_');
+
+                            String ReplaceMark = "";
+
+                            for (int k = 0; k < ExistingCharaterRussia.Length; k++)
                             {
-                                var fromEncodind = Encoding.GetEncoding("ISO-8859-1");//из какой кодировки
-                                var bytes = fromEncodind.GetBytes(cash);
-                                var toEncoding = Encoding.GetEncoding(1251);//в какую кодировку
-
-                                cash = toEncoding.GetString(bytes);
-
-                                while (cash.IndexOf("<FNC1>") != -1)
-                                {
-                                    cash = cash.Replace("<FNC1>", "и");
-                                }
-
-                                cash = cash.Remove(cash.IndexOf('>'), cash.IndexOf('<') - cash.IndexOf('>') + 1);
-
-                                String[] ExistingCharaterEnglish = new String[] { "A", "a", "B", "C", "c", "E", "e", "H", "K", "M", "O", "o", "P", "p", "T" };
-
-                                String[] ExistingCharaterRussia = new String[] { "А", "а", "В", "С", "с", "Е", "е", "Н", "К", "М", "О", "о", "Р", "р", "Т" };
-
-                                String[] Temp = cash.Split('_');
-
-                                String ReplaceMark = "";
-
-                                for (int k = 0; k < ExistingCharaterRussia.Length; k++)
-                                {
-                                    ReplaceMark = Temp[2].Replace(ExistingCharaterRussia[k], ExistingCharaterEnglish[k]);
-                                }
-
-                                String[] Splitter = Temp[1].Split('и');
-
-                                while (Splitter[0][0] == '0')
-                                {
-                                    Splitter[0] = Splitter[0].Remove(0, 1);
-                                }
-
-                                if (Splitter.Length != 1)
-                                {
-                                    Temp[1] = Splitter[0] + "и" + Splitter[1];
-                                }
-                                else
-                                {
-                                    Temp[1] = Splitter[0];
-                                }
-
-                                cash = Temp[0] + "_" + Temp[1] + "_" + ReplaceMark + "_" + Temp[3] + "_" + Temp[4] + "_" + Temp[5];
-
-                                return cash;
+                                ReplaceMark = Temp[2].Replace(ExistingCharaterRussia[k], ExistingCharaterEnglish[k]);
                             }
+
+                            String[] Splitter = Temp[1].Split('и');
+
+                            while (Splitter[0][0] == '0')
+                            {
+                                Splitter[0] = Splitter[0].Remove(0, 1);
+                            }
+
+                            if (Splitter.Length != 1)
+                            {
+                                Temp[1] = Splitter[0] + "и" + Splitter[1];
+                            }
+                            else
+                            {
+                                Temp[1] = Splitter[0];
+                            }
+
+                            cash = Temp[0] + "_" + Temp[1] + "_" + ReplaceMark + "_" + Temp[3] + "_" + Temp[4] + "_" + Temp[5];
+
+                            source.Dispose();
+                            bmp.Dispose();
+
+                            return cash;
                         }
+                        bmp.Dispose();
                     }
-                    cash = "Не правильно";
-                    return cash;
-
                 }
+                source.Dispose();
+                cash = "Не правильно";
+                return cash;
             }
             catch (Exception e)
             {
